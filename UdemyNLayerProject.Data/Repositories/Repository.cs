96a55f9369676eb61cad15ba658da1c2f1854fe6
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-02-15 17:44:00 +0300

    Ders 31:ProductRepository.cs class’sının oluşturulması

--- UdemyNLayerProject.Data/Repositories/Repository.cs
+++ UdemyNLayerProject.Data/Repositories/Repository.cs
@@ -9,10 +9,10 @@ using UdemyNLayerProject.Core.Repositories;
 
 namespace UdemyNLayerProject.Data.Repositories
 {
-    internal class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
     {
-        public readonly DbContext _context;
-        public readonly DbSet<TEntity> _dbSet;
+        protected readonly DbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
 
         public Repository(DbContext context)
         {
